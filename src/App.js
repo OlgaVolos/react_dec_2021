0c2925b0f8fbe8bd00f7dbf495589473bff405c8
OlgaVolos <72765623+OlgaVolos@users.noreply.github.com>
2022-04-20 20:39:17 +0300

    reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import './App.css';
+import {useReducer} from "react";
+
+const init =(initCount) => {
+  return {
+    counter1: initCount,
+    counter2: initCount
+  }
+}
+const reducer = (state, action) => {
+  switch (action.type){
+    case 'inc':
+      return{...state, counter1: state.counter1+1}
+    case 'dec':
+      return{...state, counter1: state.counter1-1}
+    case 'reset':
+      return{...state, counter1: 0}
+    case 'set':
+      return {state, counter1: action.payload}
+  }
+
+
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, 0, init);
+
+  // const inc = () => {
+  //   dispatch({type: 'inc'})
+  // }
   return (
     <div >
+      <div>
+        <div>{state.counter1}</div>
+        <button onClick={() => dispatch({type: 'inc'})}>inc</button>
+        <button onClick={() => dispatch({type: 'dec'})}>dec</button>
+        <button onClick={() => dispatch({type: 'reset'})}>reset</button>
+        <button onClick={() => dispatch({type: 'set', payload:10})}>setTo10</button>
+      </div>
 
     </div>
   );
 }
 
 export default App;
+//state значення чогось, що буде змінюватися
+// dispatch змінює значення state, змінюється за допомогою команд
+// const inc = () => {
+//     dispatch({type: 'inc'})
+//   } можна писати для кожного функцыъ, а можна одразу передати в кнопку
